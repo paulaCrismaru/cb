f3a1ec187e3aa42e124d223946b7647ea346b839
Adrian Suhov <asuhov@cloudbasesolutions.com>
2017-01-10 15:11:07 +0200

    Added tcpdump package for fedora

--- AIO.sh
+++ AIO.sh
@@ -450,7 +450,7 @@ if is_fedora ; then
 	fi
 
     echo "Installing packages..." >> summary.log
-    PACK_LIST=(openssh-server dos2unix at net-tools gpm bridge-utils btrfs-progs xfsprogs ntp crash bc selinux-policy-devel
+    PACK_LIST=(openssh-server dos2unix at net-tools gpm bridge-utils btrfs-progs xfsprogs ntp crash bc selinux-policy-devel tcpdump
         libaio-devel libattr-devel keyutils-libs-devel nano kexec-tools device-mapper-multipath expect sysstat git bc numactl python3)
     for item in ${PACK_LIST[*]}
     do
